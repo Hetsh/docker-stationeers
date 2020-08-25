332037beac54a57185391047465d84d58f09e7a7
Hetsh <git@hetsh.de>
2020-08-25 13:05:31 +0200

    Added upload option

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ cd "$CWD"
 
 # Load helpful functions
 source libs/common.sh
+source libs/docker.sh
 
 # Check access to docker daemon
 assert_dependency "docker"
@@ -20,30 +21,37 @@ fi
 
 # Build the image
 APP_NAME="stationeers"
-APP_TAG="hetsh/$APP_NAME"
-docker build --tag "$APP_TAG" --tag "$APP_TAG:$(git describe --tags --abbrev=0)" .
+IMG_NAME="hetsh/$APP_NAME"
+docker build --tag "$IMG_NAME" --tag "$IMG_NAME:$(git describe --tags --abbrev=0)" .
 
 # Start the test
-if [ "${1-}" = "--test" ]; then
-	# Set up temporary directory
-	TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
-	add_cleanup "rm -rf $TMP_DIR"
+case "${1-}" in
+	"--test")
+		# Set up temporary directory
+		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
+		add_cleanup "rm -rf $TMP_DIR"
 
-	# Apply permissions, UID matches process user
-	extract_var APP_UID "Dockerfile" "\K\d+"
-	chown -R "$APP_UID":"$APP_UID" "$TMP_DIR"
+		# Apply permissions, UID matches process user
+		extract_var APP_UID "Dockerfile" "\K\d+"
+		chown -R "$APP_UID":"$APP_UID" "$TMP_DIR"
 
-	# Start the test
-	extract_var DATA_DIR "Dockerfile" "\"\K[^\"]+"
-	docker run \
-	--rm \
-	--tty \
-	--interactive \
-	--publish 27500:27500/udp \
-	--publish 27500:27500/tcp \
-	--publish 27015:27015/udp \
-	--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--name "$APP_NAME" \
-	"$APP_NAME"
-fi
+		# Start the test
+		extract_var DATA_DIR "Dockerfile" "\"\K[^\"]+"
+		docker run \
+		--rm \
+		--tty \
+		--interactive \
+		--publish 27500:27500/udp \
+		--publish 27500:27500/tcp \
+		--publish 27015:27015/udp \
+		--mount type=bind,source="$TMP_DIR",target="$DATA_DIR" \
+		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+		--name "$APP_NAME" \
+		"$IMG_NAME"
+	;;
+	"--upload")
+		if ! tag_exists "$IMG_NAME"; then
+			docker push "$IMG_NAME"
+		fi
+	;;
+esac
--- update.sh
+++ update.sh
@@ -20,12 +20,12 @@ assert_dependency "curl"
 update_image "hetsh/steamcmd" "SteamCMD" "false" "(\d+\.)+\d+-\d+"
 
 # Stationeers
-RS_PKG="MANIFEST_ID" # Steam depot id for identification
-RS_REGEX="\d{17,19}"
-CURRENT_RS_VERSION=$(cat Dockerfile | grep -P -o "(?<=$RS_PKG=)$RS_REGEX")
-NEW_RS_VERSION=$(curl --silent --location "https://steamdb.info/depot/600762" | grep -P -o "(?<=<td>)$RS_REGEX(?=</td>)" | tail -n 1)
+MAN_ID="MANIFEST_ID" # Steam depot id for identification
+MAN_REGEX="\d{17,19}"
+CURRENT_RS_VERSION=$(cat Dockerfile | grep -P -o "(?<=$MAN_ID=)$MAN_REGEX")
+NEW_RS_VERSION=$(curl --silent --location "https://steamdb.info/depot/600762" | grep -P -o "(?<=<td>)$MAN_REGEX(?=</td>)" | tail -n 1)
 if [ "$CURRENT_RS_VERSION" != "$NEW_RS_VERSION" ]; then
-	prepare_update "$RS_PKG" "Stationeers" "$CURRENT_RS_VERSION" "$NEW_RS_VERSION"
+	prepare_update "$MAN_ID" "Stationeers" "$CURRENT_RS_VERSION" "$NEW_RS_VERSION"
 	update_version "$NEW_RS_VERSION"
 fi
 
